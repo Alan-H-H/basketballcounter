72f4f42357f3bb40886f13f940f862aa350412f6
Alan-H-H <hackbarttalan@gmail.com>
2023-02-02 22:14:59 -0300

    commit to branch

---
+++ index.js
@@ -0,0 +1,32 @@
+let countEl = document.getElementById("home")
+let count = 0
+
+function increment1(){
+    count = count + 1
+    countEl.textContent = count
+}
+function increment2(){
+    count = count + 2
+    countEl.textContent = count
+}
+function increment3(){
+    count = count + 3
+    countEl.textContent = count
+}
+let countElaway = document.getElementById("away")
+let count2 = 0
+
+function incrementguest1(){
+    count2 = count2 + 1
+    countElaway.textContent = count2
+}
+
+function incrementguest2(){
+    count2 = count2 + 2
+    countElaway.textContent = count2
+}
+function incrementguest3(){
+    count2 = count2 + 3
+    countElaway.textContent = count2
+}
+
